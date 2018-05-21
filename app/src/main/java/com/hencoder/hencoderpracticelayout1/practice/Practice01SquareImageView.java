020fb49a433a1ec2c5baa2907a5aaf709832588d
handler <314173730@qq.com>
2018-05-21 15:15:11 +0800

    上传2-1布局基础和apk，以及练习 这个项目貌似并不能体现出测量和布局的过程。

--- app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -27,11 +27,23 @@ public class Practice01SquareImageView extends ImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
         // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
-
         // 然后通过计算，让宽度和高度一致
+        if (measuredWidth > measuredHeight) {
+            measuredWidth = measuredHeight;
+        } else {
+            measuredHeight = measuredWidth;
+        }
 
+        /*
+        * 这里的逻辑是因为外面的逻辑代码，因为外面viewgroup的逻辑是根据seekbar的值来进行改变的。
+        * 同时FrameLayout的宽高是写的死的值，所以才会有这样的问题
+        * */
+//        measuredWidth = measuredHeight;
+
+        setMeasuredDimension(measuredWidth, measuredHeight);
         // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
     }
 }
